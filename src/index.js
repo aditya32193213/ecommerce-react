d5dbd1261ac17eb20dead9a896f2f0e9980e0a2d
Aditya Kumar <aditya32193213@gmail.com>
2025-08-15 17:24:35 +0530

    Setup GitHub Pages deployment

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {BrowserRouter} from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import { Provider } from 'react-redux';
 import store from './redux/store'
 import 'aos/dist/aos.css';
@@ -9,9 +9,9 @@ import 'aos/dist/aos.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Provider store={store}>
-    <BrowserRouter>
+    <Router>
       <App />
-    </BrowserRouter>
+    </Router>
     </Provider>
 
 );
